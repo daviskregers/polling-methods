40d56b5b3bbaf667a7417761a50acb32fd380520
Dāvis Krēgers <davis@image.lv>
2016-12-12 19:17:53 +0200

    vare method

--- polling-methods/mainwindow.cpp
+++ polling-methods/mainwindow.cpp
@@ -1,11 +1,22 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QTableWidgetItem* ptwi;
+
+    for( int i = 0; i < 20; i++) {
+        for( int j = 0; j < 20; j++) {
+            ptwi = new QTableWidgetItem(table[i][j]);
+            ui->tableWidget->setItem(i,j,ptwi);
+        }
+    }
+
 }
 
 MainWindow::~MainWindow()
@@ -14,15 +25,217 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::attachText(QString text) {
+    ui->textEdit->setText( text + "\n" + ui->textEdit->toPlainText() );
+}
 
-    ui->textEdit->setText( ui->textEdit->toPlainText() + "\n" + text );
+bool MainWindow::preprocess_data() {
+    attachText("Processing input data");
+
+    this->n = 0;
+    this->m = 0;
+
+    int maxCol = 19;
+    int maxRow = 19;
+
+    // reading
+
+    for( int i = 0; i < maxRow; i++ ) {
+
+        if(ui->tableWidget->item(i, 0)->text() != "") {
+            this->n++;
+            this->m = 0;
+        }
+        else {
+            maxRow = i + 1;
+            break;
+        }
+
+        bool found_attr = false;
+
+        for(int j = 0; j < maxCol; j++) {
+
+            if(ui->tableWidget->item(i, j)->text() != "") {
+                this->m++;
+                table[i][j] = ui->tableWidget->item(i, j)->text();
+            }
+            else {
+                maxCol = j + 1;
+                break;
+            }
+
+        }
+
+    }
+
+    // validation
+
+    bool hasDuplicate = false;
+    int j = 0, i = 0;
+    for(int j = 0; j <= this->n && !hasDuplicate; j++) { // each alternative
+
+        QString valid[20] = {};
+
+        for(int i = 0; i <= this->m && !hasDuplicate; i++) { // each expert
+
+            for(int k = 0; k < i && !hasDuplicate; k++) { // duplicate search
+
+                if(valid[k] == table[i][j] && table[i][j] != "") {
+                    attachText( "duplicate found on ("+ QString::number(i+1) +";e" + QString::number(j+1) +"); char: "+ ui->tableWidget->item(i,j)->text() );
+                    hasDuplicate = true;
+                }
+                else {
+                    valid[i] = table[i][j];
+                }
+
+            }
+
+
+        }
+    }
+
+
+    attachText("Read "+ QString::number(this->n) +" alternatives");
+    attachText("Read "+ QString::number(this->m) +" experts");
+
+    // attributes
+    if(!hasDuplicate) {
+
+        for( int j = 0; j < 20; j++ ) {
+            this->attributes[j] = "";
+        }
+
+        for( int j = 0; j < 20; j++ ) {
+
+            if(ui->tableWidget->item(0, j)->text() != "") {
+                this->attributes[j] = ui->tableWidget->item(0, j)->text();
+            }
+
+        }
+
+        QString attribs = "";
+        for( int j = 0; j < 20; j++ ) {
+            if(attributes[j] != "") {
+                attribs = attribs + attributes[j] + ", ";
+            }
+        }
+
+        attachText("Alternatives: " + attribs);
+
+    }
+
+    return !hasDuplicate;
 
 }
 
-void MainWindow::on_pushButton_clicked()
-{
+void MainWindow::do_vare() {
+    attachText("Using vare method");
+
+    QString occurences[20] = {};
+    QString discarded = "";
+    bool finished = false;
+    int iters = 0;
+
+        while(!finished) {
+            iters++;
+
+            if( iters > 50 ) {
+                attachText("50 Max iterations exceeded - stopping");
+                break;
+            }
+
+            for( int i = 0; i < 20; i++) {
+                occurences[i] = "";
+            }
+
+            for( int i = 0; i < 20; i++) { // each column
+                if(table[0][i] == "") break;
+
+                for( int k = 0; k < 20; k++) {
+
+                    if(table[k][i] == "") break;
+
+                    // check if discarded
+                    if( discarded.contains(table[k][i], Qt::CaseInsensitive) ) {
+                        continue; // increase row
+                    }
+                    else {
+
+                        bool found = false;
+                        // not discarded, check if not already occured
+                        for(int j = 1; j < 20; j++) {
+
+                            if( occurences[j].contains(table[k][i]) ) {
+                                occurences[j].remove(table[k][i], Qt::CaseInsensitive);
+                                occurences[j+1] = occurences[j+1] + table[k][i];
+                                found = true;
+                                break;
+                            }
+
+                        }
+
+                        if(!found) {
+                            occurences[1] = occurences[1] + table[k][i];
+                        }
+                        break;
+
+                    }
+
+                }
+
+            }
+
+            QString votes = "";
+            QString discard = "";
+            int maxVal = 0;
+            for( int i = 0; i < 20; i++) {
+                if(occurences[i] != "") {
+                    maxVal = i;
+                    discard.append(occurences[i]);
+                    votes = votes + " ["+ QString::number(i) +"] => " + occurences[i] + "\n";
+                }
+            }
+
+            attachText("Step 1: following highest votes casted: \n" + votes);
+            qDebug() << "maxVal: " << QString::number(maxVal);
+            qDebug() << "maxValval: " << occurences[maxVal];
+            qDebug() << "maxVallen: " << occurences[maxVal].length();
+
+            if(occurences[maxVal].length() > 1) {
+
+                discard.remove(occurences[maxVal], Qt::CaseInsensitive);
+                discarded.append(discard);
+
+                attachText("Step 2: discard lowest votes: " + discard);
+
+            }
+            else {
+                attachText("Step 2: found the winner! It is: " + occurences[maxVal]);
+                finished = true;
+            }
+
+        }
+
+
+}
+
+void MainWindow::do_process() {
+
+    if(preprocess_data()) {
+
+        if(ui->comboBox->currentIndex() == 0) {
+            do_vare();
+        }
+
+    }
+
+
+
+}
+
+void MainWindow::on_pushButton_clicked() {
 
     attachText("\nStart processing");
+    do_process();
     attachText("Processing finished\n");
 
 }
--- polling-methods/mainwindow.h
+++ polling-methods/mainwindow.h
@@ -15,7 +15,27 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+    QString attributes[20];
+
+    QString table[20][20] = {
+       {"x", "y", "z", "x", "z", "w"},
+       {"y", "z", "y", "t", "t", "z"},
+       {"z", "x", "t", "u", "v", "u"},
+       {"v", "t", "u", "v", "u", "v"},
+       {"w", "u", "x", "w", "x", "t"},
+       {"u", "w", "v", "y", "y", "y"},
+       {"t", "v", "w", "z", "w", "x"}
+    };
+
+    int n = 0, // table i length (when filled)
+        m = 0; // table j length (when filled)
+
     void attachText(QString text);
+    void do_process();
+    bool preprocess_data();
+    void test_data();
+
+    void do_vare();
 
 private slots:
     void on_pushButton_clicked();
